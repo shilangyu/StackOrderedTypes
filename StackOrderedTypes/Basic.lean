857398531e12a56600fda029552b94bc219c17cb
shilangyu <xmarcinmarcin@gmail.com>
2025-07-12 16:21:50 +0200

    Add typing rules

--- StackOrderedTypes/Basic.lean
+++ StackOrderedTypes/Basic.lean
@@ -8,6 +8,19 @@ mutual
   | function : TyCtx -> TyCtx -> Ty
 end
 
+notation t ":::" ctx => TyCtx.push t ctx
+instance : Append TyCtx where
+  append := f
+  where
+    f
+    | TyCtx.empty, b => b
+    | TyCtx.push t ctx, b => TyCtx.push t (f ctx b)
+def TyCtx.nth (ctx : TyCtx) (n : Nat) : Option Ty :=
+  match ctx, n with
+  | TyCtx.push t _, 0 => some t
+  | TyCtx.push _ ctx, n + 1 => ctx.nth n
+  | TyCtx.empty, _ => none
+
 inductive BinOp where
 | plus
 | times
@@ -37,7 +50,7 @@ notation t "::" s => Stack.push t s
 def Stack.nth (s : Stack) (n : Nat) : Option Term :=
   match s, n with
   | t :: _, 0 => some t
-  | _ :: s, n + 1 => Stack.nth s n
+  | _ :: s, n + 1 => s.nth n
   | [], _ => none
 
 inductive Reduce : (Stack × Term) -> Stack -> Prop where
@@ -45,8 +58,16 @@ inductive Reduce : (Stack × Term) -> Stack -> Prop where
 | bin_op : Reduce (.number n2 :: .number n1 :: s, .bin_op op) (BinOp.eval op n1 n2 :: s)
 | function : Reduce (s, .function ctx body) (.function ctx body :: s)
 | app : Reduce (s, body) s' -> Reduce (.function ctx body :: s, .app) s'
-| dup : Stack.nth s i = some t -> Reduce (s, .dup i) (t :: s)
+| dup : s.nth i = some t -> Reduce (s, .dup i) (t :: s)
 | seq : Reduce (s1, t1) s2 -> Reduce (s2, t2) s3 -> Reduce (s1, t1 ; t2) s3
 
-notation s "|" t " ==> " s' => Reduce (s, t) s'
-notation t " ⇓ " s => [] | t ==> s
+notation s "‖" t " ==> " s' => Reduce (s, t) s'
+notation t " ⇓ " s => [] ‖ t ==> s
+
+inductive Typing : TyCtx -> Term -> TyCtx -> Prop where
+| number : Typing ctx (.number n) (.number ::: ctx)
+| bin_op : Typing (.number ::: .number ::: ctx) (.bin_op op) (.number ::: ctx)
+| function : Typing ctx1 body ctx2 -> Typing ctx (.function ctx1 body) (.function ctx1 ctx2 ::: ctx)
+| app : Typing (.function ctx1 ctx2 ::: ctx1 ++ ctx) .app (ctx2 ++ ctx)
+| dup : ctx.nth i = some T -> Typing ctx (.dup i) (T ::: ctx)
+| seq : Typing ctx1 t1 ctx2 -> Typing ctx2 t2 ctx3 -> Typing ctx1 (t1 ; t2) ctx3
